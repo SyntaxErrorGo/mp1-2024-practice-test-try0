46726813bcedc2255cdfc1c28ba3fdabe22928bd
SyntaxErrorGo <ila.26russ@gmail.com>
2024-12-05 09:30:10 +0300

    Update task1.c
    Юнкин Илья Викторович

--- 3824Б1ПР3/29 Юнкин Илья Викторович/task1.c
+++ 3824Б1ПР3/29 Юнкин Илья Викторович/task1.c
@@ -1,7 +1,35 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <stdio.h>
+#include <limits.h>
+
+int sum_of_two_numbers(int a, int b) {
+    
+    if (a > 0 && b > 0 && a >= INT_MAX - b) {
+        return INT_MAX; 
+    }
+    
+    if (a < 0 && b < 0 && a <= INT_MIN - b) {
+        return INT_MIN; 
+    }
+    return 0; 
+}
+
+int main() {
+    int a;
+    int b;
+    printf("Enter a number(a b)");
+    scanf("%d %d", &a, &b);
+    int result = sum_of_two_numbers(a, b);
+
+    if (result == INT_MAX) {
+        printf("a + b >= INTMAX. Result: INT_MAX\n");
+    } else if (result == INT_MIN) {
+        printf("a + b <= INT_MIN. Result: INT_MIN\n");
+    } else {
+        printf("0: %d\n", result);
+    }
+
+
 }
